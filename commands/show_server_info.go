ff492b55d444f89cbd14c33fc5493c59faf26767
koitra <koitra@rhoti.com>
2026-05-18 09:55:19 +0300

    fix: decode int 64 from string

--- commands/show_server_info.go
+++ commands/show_server_info.go
@@ -18,25 +18,25 @@ type (
 		SearchKeywords        string `json:"SEARCHKEYWORDS_s"`
 		GameVersion           string `json:"GameVersion_s"`
 		Licensed              bool   `json:"LICENSEDSERVER_b"`
-		Playtime              int64  `json:"PLAYTIME_I"`
-		Flags                 int64  `json:"Flags_I"`
+		Playtime              int64  `json:"PLAYTIME_I,string"`
+		Flags                 int64  `json:"Flags_I,string"`
 		MatchHopper           string `json:"MATCHHOPPER_s"`
 		MatchTimeout          int64  `json:"MatchTimeout_d"`
 		SessionTemplateName   string `json:"SESSIONTEMPLATENAME_s"`
 		Password              bool   `json:"Password_b"`
-		PlayerCount           int64  `json:"PlayerCount_I"`
+		PlayerCount           int64  `json:"PlayerCount_I,string"`
 		TagLanguages          string `json:"TagLanguage_s"`
 		Region                string `json:"Region_s"`
-		CurrentModLoadedCount int64  `json:"CurrentModLoadedCount_I"`
+		CurrentModLoadedCount int64  `json:"CurrentModLoadedCount_I,string"`
 		AllModsWhitelisted    bool   `json:"AllModsWhitelisted_b"`
 		Team1                 string `json:"TeamOne_s"`
 		Team2                 string `json:"TeamTwo_s"`
 		SessionPassword       string `json:"SessionPassword_s"`
-		PlayerReserveCount    int64  `json:"PlayerReserveCount_I"`
-		PublicQueueLimit      int64  `json:"PublicQueueLimit_I"`
-		PublicQueue           int64  `json:"PublicQueue_I"`
-		ReservedQueue         int64  `json:"ReservedQueue_I"`
-		BeaconPort            int64  `json:"BeaconPort_I"`
+		PlayerReserveCount    int64  `json:"PlayerReserveCount_I,string"`
+		PublicQueueLimit      int64  `json:"PublicQueueLimit_I,string"`
+		PublicQueue           int64  `json:"PublicQueue_I,string"`
+		ReservedQueue         int64  `json:"ReservedQueue_I,string"`
+		BeaconPort            int64  `json:"BeaconPort_I,string"`
 		NextLayer             string `json:"NextLayer_s"`
 	}
 )
